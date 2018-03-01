f1b53f252104bccdef5114bc8affb9b2103d2ef7
jonathanBuhler <benedict.buhler@gmail.com>
2018-03-01 20:05:47 +0000

    Added Car class

--- main.py
+++ main.py
@@ -17,17 +17,51 @@ class Ride:
     def __str__(self):
         return "Ride Number: " + str(self.rideN)
 
+class Car:
+    carCount = 0
+
+    def __init__(self):
+        self.location = (0,0)
+        self.destination = (0,0)
+        self.busy = False
+        Ride.rideCount += 1
+
+    def move(self):
+        if self.location[0] != self.destination[0]:
+            if self.location[0] < self.destination[0]:
+                self.location[0] += 1
+            else:
+                self.location[0] -= 1
+        elif self.location[1] != self.destination[1]:
+            if self.location[1] < self.destination[1]:
+                self.location[1] += 1
+            else:
+                self.location[1] -= 1
+
+    def changeState(self):
+        self.busy = not self.busy
+
 def formatData(file):
     global rides
+    global cars
+
     imRides = []
+    imCars = []
+
     rawData = np.genfromtxt(file)
     rideData = np.delete(rawData, 0, axis = 0)
+
     for index, ride in enumerate(rideData):
         newRide = Ride(index, (ride[0], ride[1]), (ride[2], ride[3]), ride[4], ride[5])
         imRides.append(newRide)
 
     rides = np.asarray(imRides)
     rides = np.sort(rides, kind = 'heapsort')
+
+    for i in range(int(rawData.item((0, 2)))):
+        imCars.append(Car())
+
+    cars = np.asarray(imCars)
 
 if (__name__ == '__main__'):
     formatData('a_example.in')
